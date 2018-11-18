5fbf23451861b656b589ef2d663f2e8153236a41
1386studio <1386studio@gmail.com>
2018-11-18 08:20:15 +0800

    editor add

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var conflicts = require('../model/conflict.js');
 var alerts = require('../model/alert.js');
+var _ = require('lodash');
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/:id?', function(req, res, next) {
   mongoose.connect("mongodb://localhost/gct", function(err) {
     if(!err){
       conflicts.find(function(err,docs){
-        res.render('index', { title: 'Express', conflicts: docs});
+        var index = req.params.id ? _.findIndex(docs, function(o) { return o._id == req.params.id; }) : 0;
+        index = index >= 0 ? index : 0;
+        res.render('index', { title: 'Express', conflicts: docs, selected: docs[index]});
       })
     }
   });
